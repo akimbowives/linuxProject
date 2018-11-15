cbe2654141e87fcb51dde60741332b2d773eddc3
Braxden <bth0331@westminstercollege.edu>
2018-11-15 11:25:11 -0700

    Commiting a partial version of my work,  not 100% complete but I think the groundwork is there

--- test2.sh
+++ test2.sh
@@ -1,2 +1,35 @@
-#Inital Commit to verify that gitHub is working
+#Script must 1st generate index.html then create A.html and hyperlink to A.hml in index
+#Place index.html into the folder /var/www/html
+#Place a.html into the folder /var/www/html/A
+#Both index and A must have bodies, but the content is up to me
 
+#Creates index.html
+echo "<html>
+	<head>
+	<title>
+	I hope this Works
+	</title>
+	<body>
+	<a href="A.html">Link to A</a>
+	</body>
+	</html>	" >> index.html
+
+#Creates A.html
+echo "<html>
+	<head>
+	<title>
+	Welcome to A!
+	</title>
+	<body>
+	<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Click This if YOU DARE</a>
+	</body>
+	</html> " >> A.html
+
+#Moves index.html into the correct folder (/var/www/html)
+sudo mv index.html /var/www/html/
+
+#Makes the Directory A in /var/www/html in order to store A.html
+sudo mkdir /var/www/html/A	
+
+#Moves A.html into the correct folder (/var/www/html/A)
+sudo mv A.html /var/www/html/A
